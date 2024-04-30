0f2e9f4400550bc0c58b6a0b399e672588c63654
KyleQ1 <kylequinlan09@gmail.com>
2024-04-30 08:26:12 -0700

    silly me

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -10,7 +10,6 @@ def get_potions():
             sqlalchemy.text("""SELECT gp.r, gp.g, gp.b, gp.d, sum(p.quantity) as total_quantity, gp.sku, gp.price
                         FROM potion_ledger p
                         JOIN grab_potions gp ON p.grab_potion_id = gp.id
-                        WHERE p.quantity > 0
                         GROUP BY gp.r, gp.g, gp.b, gp.d, gp.sku, gp.price
                         ORDER BY total_quantity DESC""")).fetchall()
 
@@ -24,13 +23,14 @@ def get_catalog():
     selling_plan = []
     for row in result:
         r, g, b, d, quant, sku, price = row
-        selling_plan.append({
-                "sku": sku,
-                "name": f"{sku} potion",
-                "quantity": quant,
-                "price": price,
-                "potion_type": [r, g, b, d],
-            })
+        if quant > 0:
+            selling_plan.append({
+                    "sku": sku,
+                    "name": f"{sku} potion",
+                    "quantity": quant,
+                    "price": price,
+                    "potion_type": [r, g, b, d],
+                })
 
     selling_plan = sorted(selling_plan, key = lambda inventory : (inventory["price"], -inventory["quantity"]))
     print("Selling Plan: ", selling_plan[:6])
